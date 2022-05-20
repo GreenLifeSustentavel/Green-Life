20309fd59b49d6d767e4df1ddf38908f21c185b7
GreenLifeSustentavel <greenlifisustentavel@gmail.com>
2022-05-20 15:54:35 -0300

    inseri a logo, atualizei o css do menu, separei os arquivos em modulos para deixar o codigo limpo, criei a estrutura da pagina de topicos sobre vida sustentavel

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
 
+const axios = require("axios");
+
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
@@ -11,5 +13,27 @@ app.use(express.json());
 app.get("/", function(req, res){
     res.render("index.ejs");
 });
+
+app.post("/newUser", function(req, res){
+    const nome = req.body.nome;
+    const email = req.body.email;
+    const senha = req.body.senha;
+
+    axios.post('http://localhost:7000/usuarios', {
+        nome: nome,
+        email: email,
+        senha: senha
+    }).then(resp => {
+        console.log(resp.data);
+        console.log("==========================================================================================================");
+    }).catch(erro => {
+        console.log(erro);
+        console.log("==========================================================================================================");
+    });
+});
+
+app.get("/vidaSustentavel", function(req, res){
+    res.render("vidaSustentavel.ejs");
+})
 
 app.listen(3000);
