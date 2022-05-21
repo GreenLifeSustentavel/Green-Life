07b19640c294039b625e133ea8b6476c707742f0
GreenLifeSustentavel <greenlifisustentavel@gmail.com>
2022-05-21 19:26:34 -0300

    Proximo: ao fazer um cad/log redirecionar para uma função main e criar um token no armazenamento interno do usuario atraves do fs pelo front end para verificar login sempre que necessario

--- index.js
+++ index.js
@@ -10,14 +10,19 @@ app.use(express.static('public'));
 app.use(express.urlencoded());
 app.use(express.json());
 
+var log = false;
+var nome = "";
+var email = "";
+var senha = "";
+
 app.get("/", function(req, res){
     res.render("index.ejs");
 });
 
 app.post("/newUser", function(req, res){
-    const nome = req.body.nome;
-    const email = req.body.email;
-    const senha = req.body.senha;
+    nome = req.body.nome;
+    email = req.body.email;
+    senha = req.body.senha;
 
     axios.post('http://localhost:7000/usuarios', {
         nome: nome,
@@ -26,11 +31,58 @@ app.post("/newUser", function(req, res){
     }).then(resp => {
         console.log(resp.data);
         console.log("==========================================================================================================");
+        log = true;
+        res.redirect("/");
     }).catch(erro => {
         console.log(erro);
         console.log("==========================================================================================================");
+        nome = "";
+        email = "";
+        senha = "";
     });
 });
+
+app.post("/login", function(req, res){
+    var emailLogin = req.body.email;
+    var senhaLogin = req.body.senha;
+
+    axios.get(`http://localhost:7000/usuarios/?email=${emailLogin}&senha=${senhaLogin}`).then(resp => {
+        if(resp.data === undefined){
+            
+        } else {
+            console.log(resp.data[0]);
+            console.log("==========================================================================================================");
+            log = true;
+            nome = resp.data[0].nome;
+            email = resp.data[0].email;
+            senha = resp.data[0].senha;
+            res.redirect("/");
+        }
+        
+    }).catch(erro => {
+        console.log("==========================================================================================================");
+        console.log(erro);
+        console.log("==========================================================================================================");
+        log = false;
+        nome = "";
+        email = "";
+        senha = "";
+        res.send("<html><head></head><body><script>alert(\"credenciais incorretas\");window.open(document.referrer,'_self')</script></body></html>");
+    })
+});
+
+app.get("/perfil", function(req, res){
+    res.render("perfil.ejs");
+});
+
+app.get("/log", function(req, res){
+    res.send(log);
+});
+
+app.get("/deslog", function(req, res){
+    log = false;
+    res.redirect("/");
+})
 
 app.get("/vidaSustentavel", function(req, res){
     res.render("vidaSustentavel.ejs");
